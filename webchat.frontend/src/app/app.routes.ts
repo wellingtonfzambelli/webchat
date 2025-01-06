1761d656f68a4d034b8c3a5b2c4e7b1101ef0beb
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2025-01-06 20:31:40 -0300

    creating template pages

--- webchat.frontend/src/app/app.routes.ts
+++ webchat.frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './features/chat/chat.component';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'chat', component: ChatComponent /*, canActivate:[authGuard]*/ }
+];
