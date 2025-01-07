a1a7fcda01b2d4b8288f188c7c7d7187bddc2ef6
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2025-01-06 21:00:31 -0300

    creating home page validation

--- webchat.frontend/src/app/features/home/home.component.ts
+++ webchat.frontend/src/app/features/home/home.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  private _formBuilder = inject(FormBuilder);
+  
+  public submitted = false;
+  
+  public loginForm = this._formBuilder.group({
+    userName: new FormControl('', [Validators.required]),
+    gender: new FormControl('', [Validators.required]),
+  });
+
+  public onSubmit() {
+    if (this.loginForm.invalid) {
+      this.submitted = true;
+    }else{
+      this.submitted = false;
+      //this.loginForm.reset();
+    }
+  }
 }
