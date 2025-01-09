4a01d48f2a9924300878d5600d311513795cc49b
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2025-01-09 15:42:34 -0300

    env adjust

--- webchat.frontend/src/environments/environment.ts
+++ webchat.frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseUrlChatApi: 'https://poc-webchat-frb2cqhububchadq.brazilsouth-01.azurewebsites.net/chat',
-    signalHubUrl: "https://poc-webchat-frb2cqhububchadq.brazilsouth-01.azurewebsites.net/hub/chat"
+    baseUrlChatApi: 'chat',
+    signalHubUrl: "hub/chat"
 };
